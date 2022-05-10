5e18cd9d49ad7d310d410e05f5546427632fa4e8
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-10 12:10:33 -0700

    first test failed

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,21 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { renderStadiums } from '../utils.js';
+import { stadiums } from '../stadiums.js';
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('test stadium render function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = `<div class="stadium"><h1>Providence Park</h1><img src="./assets/providence.jpg"><p>Providence has a capacity of 26000 people and is in Portland.</p></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderStadiums(stadiums[0]);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
